426e07fb1afdc49188c0bb33f498c16c2f5e2cdb
Alvarado08 <jorgealvarado0238@gmail.com>
2023-03-26 16:53:13 -0600

    Day 21
    Finishing up second part of loop exercises in tasks section

--- javascript/fundamentals-pt4/loops2.js
+++ javascript/fundamentals-pt4/loops2.js
@@ -314,3 +314,77 @@ label: {
 //? But here we have the postfix form i++. So it increments i to 5, but returns the old value
 //? Hence the comparison is actually while(4 < 5) – true, and the control goes on to alert
 //? The value i = 5 is the last one, because on the next step while(5 < 5) is false
+
+//* Which values get shown by the "for" loop?
+//? For each loop write down which values it is going to show. Then compare with the answers
+
+//? Both loops alert same values or not?
+
+//* 1. The postfix form:
+// for (let i = 0; i < 5; i++) alert( i );
+// Output: 0 - 4 
+
+//* 2. The prefix form:
+// for (let i = 0; i < 5; ++i) alert( i );
+// Output: 0 - 4
+
+//* Output even numbers in the loop
+//? Use the for loop to output even numbers from 2 to 10
+
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log( i );
+// }
+// }
+
+//* Replace "for" with "while"
+//? Rewrite the code changing the for loop to while without altering its behavior (the output should stay same)
+
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+
+// let i = 3;
+// while (i){
+//  alert( `number ${i}!` );
+//  i--;
+//}
+
+//* Repeat until the input is correct
+//? Write a loop which prompts for a number greater than 100
+//? If the visitor enters another number – ask them to input again
+//? The loop must ask for a number until either the visitor enters a number greater than 100
+//? or cancels the input/enters an empty line
+//? Here we can assume that the visitor only inputs numbers
+//? There’s no need to implement a special handling for a non-numeric input in this task
+
+// let value;
+// while (value < 100 && value) {
+
+//   value = +prompt("Enter a number greater than 100", '');
+
+//   if (!value) break; 
+
+// }
+
+//? Answer:
+// let num;
+// do {
+//   num = prompt("Enter a number greater than 100?", 0);
+// } while (num <= 100 && num);
+//? The loop do..while repeats while both checks are truthy:
+//? 1. The check for num <= 100 – that is, the entered value is still not greater than 100
+//? 2. The check && num is false when num is null or an empty string
+//? Then the while loop stops too
+//? P.S. If num is null then num <= 100 is true
+//? so without the 2nd check the loop wouldn’t stop if the user clicks CANCEL. Both checks are required
+
+//* Output prime numbers
+//? An integer number greater than 1 is called a prime if it cannot be divided 
+//? without a remainder by anything except 1 and itself
+//? In other words, n > 1 is a prime if it can’t be evenly divided by anything except 1 and n
+//? For example, 5 is a prime, because it cannot be divided without a remainder by 2, 3 and 4
+
+//? Write the code which outputs prime numbers in the interval from 2 to n
+//? For n = 10 the result will be 2,3,5,7
+//? P.S. The code should work for any n, not be hard-tuned for any fixed value
