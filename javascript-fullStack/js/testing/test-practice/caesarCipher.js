cd2997645e47547da49ca6adea39e6c0719ca25e
Alvarado08 <jorgealvarado0238@gmail.com>
2024-03-02 23:50:11 -0600

    Caesar Cipher Testing
    Start of Testing Js section, Testing practice lesson, exercise 4.

---
+++ javascript-fullStack/js/testing/test-practice/caesarCipher.js
@@ -0,0 +1,12 @@
+/**
+ * Ceaser Cipher
+ * @param {string} message - The message to shift/encrypt
+ * @param {number} key - The key shift to cipher the message
+ * @returns {string} - The shifted/encrypted message
+ * @example
+ * caesarCipher('hello', 5) // 'mjqqt'
+ */
+
+function caesarCipher(message, key) {
+  // Write your code here
+}
